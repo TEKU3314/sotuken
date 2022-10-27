86eecb907b29677cf343e52a53aa96705beff699
TEKU3314 <teruki931420000228@gmail.com>
2022-10-27 12:10:50 +0900

    generateモデルの作成

--- aichan/generate/models.py
+++ aichan/generate/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Type(models.Model):
+    """タイプモデル"""
+    
+    name = models.CharField(max_length=25)
+
+class Word(models.Model):
+    """単語モデル"""
+
+    word = models.CharField(max_length=25)
+    count = models.IntegerField(default=0)
+    typeid = models.ForeignKey(Type,on_delete=models.PROTECT)
+
--- aichan/generate/views.py
+++ aichan/generate/views.py
@@ -1,15 +1,15 @@
 from django.views import generic
 
+from .models import Word
+from .models import Type
+
 class IndexView(generic.TemplateView):
     template_name = "index.html"
 
 class GenetateView(generic.TemplateView):
+    model = Word
     template_name = "generate.html"
 
-
-class GenerateConfirmView():
-    template_name = "generate_confirm.html"
-
-
-class GenerateFinishView():
-    template_name = "generate_finish.html"
+    def get_queryset(self):
+        result = Word.objects.raw('SELECT w.id,w.word,w.count,t.name FROM Word w INNER JOIN TYPE t ON w.id=t.id order_by t.id')
+        return result
