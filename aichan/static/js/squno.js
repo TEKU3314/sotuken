c3256a8f6ed480177a6c6376059aafc4c733388c
tabisho <shoma.tbr@gmail.com>
2022-12-02 10:40:17 +0900

    田平ごみ

--- aichan/static/js/squno.js
+++ aichan/static/js/squno.js
@@ -1,49 +1,20 @@
-var windowwidth = window.innerWidth || document.documentElement.clientWidth || 0;
-    if (windowwidth > 768){
-      var responsiveImage = [//PC用の画像
-        { src: './static/img/img_01.jpg'},
-        { src: './static/img/img_02.jpg'},
-        { src: './static/img/img_03.jpg'},
-		{ src: './static/img/img_04.jpg'},
-		{ src: './static/img/img_05.jpg'},
-		{ src: './static/img/img_06.jpg'}
-      ];
-    } else {
-    }
-
-//Vegas全体の設定
-
-$('#slider').vegas({
-    overlay: true,//画像の上に網線やドットのオーバーレイパターン画像を指定。
-    transition: 'blur',//切り替わりのアニメーション。http://vegas.jaysalvat.com/documentation/transitions/参照。fade、fade2、slideLeft、slideLeft2、slideRight、slideRight2、slideUp、slideUp2、slideDown、slideDown2、zoomIn、zoomIn2、zoomOut、zoomOut2、swirlLeft、swirlLeft2、swirlRight、swirlRight2、burnburn2、blurblur2、flash、flash2が設定可能。
-    transitionDuration: 2000,//切り替わりのアニメーション時間をミリ秒単位で設定
-    delay: 10000,//スライド間の遅延をミリ秒単位で。
-    animationDuration: 20000,//スライドアニメーション時間をミリ秒単位で設定
-    animation: 'kenburns',//スライドアニメーションの種類。http://vegas.jaysalvat.com/documentation/transitions/参照。kenburns、kenburnsUp、kenburnsDown、kenburnsRight、kenburnsLeft、kenburnsUpLeft、kenburnsUpRight、kenburnsDownLeft、kenburnsDownRight、randomが設定可能。
-    slides: responsiveImage,//画像設定を読む
+$('.slider').slick({
+  autoplay: true,//自動的に動き出すか。初期値はfalse。
+  autoplaySpeed: 3000,//次のスライドに切り替わる待ち時間
+  speed:1000,//スライドの動きのスピード。初期値は300。
+  infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+  slidesToShow: 1,//スライドを画面に3枚見せる
+  slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
+  arrows: true,//左右の矢印あり
+  prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+  nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+  dots: true,//下部ドットナビゲーションの表示
+      pauseOnFocus: false,//フォーカスで一時停止を無効
+      pauseOnHover: false,//マウスホバーで一時停止を無効
+      pauseOnDotsHover: false,//ドットナビゲーションをマウスホバーで一時停止を無効
   });
 
-var slider;
-var sliderFlag = false;
-var breakpoint = 768;//768px以下の場合
-  
-function sliderSet() {
-        var windowWidth = window.innerWidth;
-        if (windowWidth >= breakpoint && !sliderFlag) {//768px以上は1行でスライダー表示
-            slider = $('.slider').bxSlider({
-            touchEnabled:false,//リンクを有効にするためスライドをマウスでドラッグした際にスライドの切り替えを可能にする機能を無効化
-			      mode: 'vertical',//縦スライド指定
-			      controls: false,//前後のコントロールを表示させない。
-		      	  auto: 'true',//自動的にスライド
-		      	  pager: false//ページ送り無効化
-		});
-            sliderFlag = true;
-        } else if (windowWidth < breakpoint && sliderFlag) {
-            slider.destroySlider();//bxSliderのOptionであるdestroySliderを使用してスライダーの動きを除去
-            sliderFlag = false;
-        }
-    }
-
-$(window).on('load resize', function() {
-        sliderSet();
+//スマホ用：スライダーをタッチしても止めずにスライドをさせたい場合
+$('.slider').on('touchmove', function(event, slick, currentSlide, nextSlide){
+  $('.slider').slick('slickPlay');
 });
