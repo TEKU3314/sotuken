268c8a8962c7eb2a83bc9a73d0c5535b8b3fe608
tabisho <shoma.tbr@gmail.com>
2023-01-18 13:42:31 +0900

    ss

--- aichan/generate/views.py
+++ aichan/generate/views.py
@@ -1,14 +1,14 @@
 from django.views import generic
 from django.http import JsonResponse
 from django.http import HttpResponse, QueryDict
-from diffusers import StableDiffusionPipeline
+from diffusers import StableDiffusionPipeline, DDIMScheduler
 from datetime import datetime
 from PIL import Image
 
 import numpy as np
 
-import torch
-from torch import autocast
+import torch 
+from torch import autocast 
 
 import cv2
 import json
@@ -18,7 +18,8 @@ import logging
 import time
 import os
 
-from .models import Word
+from .models import BackGroundWord
+from .models import GirlWord
 from .models import Type
 from download.models import Image
 
@@ -58,7 +59,6 @@ def make_background(request):
 
     #StableDiffusionパイプライン設定
     pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", use_auth_token=HF_TOKEN)
-    #pipe = StableDiffusionPipeline.from_pretrained("hakurei/waifu-diffusion", torch_dtype=torch.float32)
     #pipe = StableDiffusionPipeline.from_pretrained("hakurei/waifu-diffusion", use_auth_token=HF_TOKEN)
     pipe.to("cpu")
 
@@ -80,9 +80,7 @@ def make_background(request):
 def make_girl(request):
     time.sleep(1)
     
-    #チェックボックスで選択したものを受け取る（name受け取り）
     dic = QueryDict(request.body, encoding='utf-8')
-
     style = dic.get('style')
     nature = dic.get('nature')
     building = dic.get('building')
@@ -92,10 +90,6 @@ def make_girl(request):
     print(nature)
     print('〇〇風')
     print(style)
-    print('動物')
-    print(animal)
-    print('海')
-    print(sea)
     print('建物')
     print(building)
     print('------------------------------------------------------')
@@ -111,68 +105,29 @@ def make_girl(request):
     if building:
         zyumon = zyumon + building + ' '
 
-    if sea:
-        zyumon = zyumon + sea + ' '
 
-    if animal:
-        zyumon = zyumon + animal + ' '
-
-
-    #HuggingFaceのトークン
     HF_TOKEN = "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"
 
-    #StableDiffusionパイプライン設定
-    pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", use_auth_token=HF_TOKEN)
-    #pipe = StableDiffusionPipeline.from_pretrained("hakurei/waifu-diffusion", torch_dtype=torch.float32)
-    print('--------------------------------------------------------------------------')
-    #pipe = StableDiffusionPipeline.from_pretrained("hakurei/waifu-diffusion", use_auth_token=HF_TOKEN)
-    #使用するデバイスを設定
-    #pipe.to("cuda")
-    pipe.to("cpu")
-    print('--------------------------------------------------------------------------')
+    pipe = StableDiffusionPipeline.from_pretrained("hakurei/waifu-diffusion",use_auth_token=HF_TOKEN,torch_dtype=torch.float16,)
+    #pipe = StableDiffusionPipeline.from_pretrained("hakurei/waifu-diffusion", )
 
-    # 生成したい画像を指示
-    # prompt = "anime,kuudere,solo,kawaii,8k,lips,beautiful blue eyes,lips,Uniform,highschool,japanese anime,highres,portrait"
+    pipe.to("cuda")
+
+
+
     prompt = zyumon
-    print('--------------------------------------------------------------------------')
 
-    #画像生成
-    print('--------------------------------------------------------------------------')
     image = pipe(prompt, height=512, width=768)["sample"][0]
-    print('--------------------------------------------------------------------------')
 
 
-    #生成した画像をファイル出力
     date = datetime.now().strftime("%Y%m%d_%H%M%S") #現在の日時を取得
     pngno = date
     filename = "./media/" + date + ".png" #ファイル名を生成した日時にする
-    print('--------------------------------------------------------------------------')
     image.save(filename)
 
-    print(filename)
-
-    #生成した画像を取り出す
-    #img = Image.open(filename)
-    print('--------------------------------------------------------------------------')
-    print('sssssss')
-    #img = cv2.imread(filename)
-    print('dddddddd')
-    #print(img)
-
-
-
-    #データベースに画像を保存
-    print('qqqqqqqqqqq')
-    
-    #生成した画像を消す
-    #os.remove(filename)
-
-    #dのresultに生成した画像のファイル名を格納する
     d = {
         'result': pngno,
     }
-
-    print('ddddddddddddd')
     return JsonResponse(d)
 
 class IndexView(generic.TemplateView):
@@ -186,7 +141,7 @@ class BackGroundView(generic.ListView):
     template_name = "background.html"
 
     def get_queryset(self):
-        result = Word.objects.raw('SELECT w.id,w.word,w.word_en,w.count,t.name FROM generate_background w INNER JOIN generate_type t ON w.typeid_id=t.id ORDER BY t.id')
+        result = BackGroundWord.objects.raw('SELECT w.id,w.word,w.word_en,w.count,t.name FROM generate_backgroundword w INNER JOIN generate_type t ON w.typeid_id=t.id ORDER BY t.id')
         return result
 
 class GirlView(generic.ListView):
@@ -194,6 +149,6 @@ class GirlView(generic.ListView):
     template_name = "girl.html"
 
     def get_queryset(self):
-        result = Word.objects.raw('SELECT w.id,w.word,w.word_en,w.count,t.name FROM generate_girl w INNER JOIN generate_type t ON w.typeid_id=t.id ORDER BY t.id')
+        result = GirlWord.objects.raw('SELECT w.id,w.word,w.word_en,w.count,t.name FROM generate_girlword w INNER JOIN generate_type t ON w.typeid_id=t.id ORDER BY t.id')
         return result
 
